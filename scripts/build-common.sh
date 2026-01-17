9100e92d509b7c90b89004e3b8f5daacd38c6e8b
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-01-17 23:37:58 +0100

    prefix

--- scripts/build-common.sh
+++ scripts/build-common.sh
@@ -5,9 +5,9 @@ trap 's=$?; echo >&2 "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
 echo RUNNING SETUP
 # WORKAROUND for empty `$@` failing with `set -u` on OSX
 if test $# -gt 0; then
-    make setup "${@}"
+    make setup "PREFIX=${PWD}/build/install" "${@}"
 else
-    make setup
+    make setup "PREFIX=${PWD}/build/install"
 fi
 
 echo RUNNING COMPILE
@@ -17,7 +17,7 @@ echo RUNNING TESTS
 make tests
 
 echo RUNNING INSTALL
-make install PREFIX="${PWD}/build/install"
+make install
 
 echo RUNNING EXAMPLES
 make -C examples EXE="${PWD}/build/src/adapterremoval3"
