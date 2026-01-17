d445963c014680ecd851b34996e32f1f8094b386
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-01-17 22:33:56 +0100

    strip

--- scripts/build-bundle.sh
+++ scripts/build-bundle.sh
@@ -10,7 +10,12 @@ mv -v "${PREFIX}/bin/adapterremoval3" "${TARGET}/"
 mv -v "${PREFIX}/share/adapterremoval3/examples" "${TARGET}/examples"
 mv -v "${PREFIX}/share/adapterremoval3/docs/html" "${TARGET}/docs"
 mv -v "${PREFIX}/share/man/man1/adapterremoval3.1" "${TARGET}/docs/"
-strip "${TARGET}/adapterremoval3*"
+
+for exe in adapterremoval3 adapterremoval3.exe; do
+    if test -e "${TARGET}/${exe}"; then
+        strip "${TARGET}/${exe}"
+    fi
+done
 
 if test -e "/ucrt64/bin/libdeflate.dll"; then
     cp "/ucrt64/bin/libdeflate.dll" "${TARGET}"
