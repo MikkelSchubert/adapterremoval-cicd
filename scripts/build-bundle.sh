832556e76309393fc7cee38d3e38db5affde56a9
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-01-17 22:59:59 +0100

    .

--- scripts/build-bundle.sh
+++ scripts/build-bundle.sh
@@ -6,16 +6,18 @@ PREFIX=$1
 TARGET=$2
 
 mkdir -p "${TARGET}"
-mv -v "${PREFIX}/bin/adapterremoval3" "${TARGET}/"
+for exe in adapterremoval3 adapterremoval3.exe; do
+    if test -e "${PREFIX}/bin/${exe}"; then
+        strip "${PREFIX}/bin/${exe}"
+        mv -v "${PREFIX}/bin/${exe}" "${TARGET}/"
+        break
+    fi
+done
+
 mv -v "${PREFIX}/share/adapterremoval3/examples" "${TARGET}/examples"
 mv -v "${PREFIX}/share/adapterremoval3/docs/html" "${TARGET}/docs"
 mv -v "${PREFIX}/share/man/man1/adapterremoval3.1" "${TARGET}/docs/"
 
-for exe in adapterremoval3 adapterremoval3.exe; do
-    if test -e "${TARGET}/${exe}"; then
-        strip "${TARGET}/${exe}"
-    fi
-done
 
 # WORKAROUND: libdeflate cannot be linked statically
 if test -e "/ucrt64/bin/libdeflate.dll"; then
