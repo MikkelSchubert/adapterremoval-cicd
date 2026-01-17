30dd3e8e1850b0e823b8b9eb8a8c8bc24251e506
Mikkel Schubert <2411627+MikkelSchubert@users.noreply.github.com>
2026-01-17 20:39:57 +0100

    .

--- scripts/setup-windows-latest.sh
+++ scripts/setup-windows-latest.sh
@@ -11,4 +11,5 @@ pacman -S --noconfirm \
     mingw-w64-ucrt-x86_64-mimalloc \
     mingw-w64-ucrt-x86_64-python \
     mingw-w64-ucrt-x86_64-python-jsonschema \
-    mingw-w64-ucrt-x86_64-python-sphinx
+    mingw-w64-ucrt-x86_64-python-sphinx \
+    mingw-w64-ucrt-x86_64-uv
